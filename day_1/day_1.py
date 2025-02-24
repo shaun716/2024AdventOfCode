78590b94cbad96744e640d1b0f0dd3b618c367e1
dmcea13 <dmcea13@wgu.edu>
2025-02-23 22:56:36 -0500

    Day 1 Completed

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,15 +1,22 @@
-fs = open("input.txt")
+# Initializing variables to be used later
+total_distance = 0
 new_list1 = []
 new_list2 = []
-total_distance = 0
 
+#Opening and parsing data from input.txt
+fs = open("input.txt")
 for i in fs:
     new_list1.append(i[8:-1])
     new_list2.append(i[0:5])
 
+#Sorting lists
 new_list1.sort()
 new_list2.sort()
 
+#Iterating through num_list1, subtracting value of new_list2 from new_list1 at each iteration to calculate distance
 for i in range(len(new_list1)):
+
+    #Result of subtraction is passed to int() and then abs()
     total_distance += abs(int(new_list1[i]) - int(new_list2[i]))
+
 print(f"Total distance = {total_distance}")
