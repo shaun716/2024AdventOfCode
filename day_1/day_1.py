045934d1a8fbca38058211bcc24c6a58d9ffb3e5
dmcea13 <dmcea13@wgu.edu>
2025-02-23 22:48:56 -0500

    Day 1 Completed

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,1 +1,15 @@
-print("Hello World")
+fs = open("input.txt")
+new_list1 = []
+new_list2 = []
+total_distance = 0
+
+for i in fs:
+    new_list1.append(i[8:-1])
+    new_list2.append(i[0:5])
+
+new_list1.sort()
+new_list2.sort()
+
+for i in range(len(new_list1)):
+    total_distance += abs(int(new_list1[i]) - int(new_list2[i]))
+print(f"Total distance = {total_distance}")
